daf6ba64c34226f67a3cb95dcb810e3d2f5715d1
nihad.hrnjic@klika.ba <nhrnjic>
2020-03-23 20:06:42 +0100

    read system status from mqtt and update UI

--- app/src/main/java/com/nhrnjic/heatingcontroller/MainActivity.java
+++ app/src/main/java/com/nhrnjic/heatingcontroller/MainActivity.java
@@ -20,6 +20,12 @@ import com.nhrnjic.heatingcontroller.model.SystemStatus;
 import com.nhrnjic.heatingcontroller.service.HeatingControlService;
 import com.nhrnjic.heatingcontroller.service.MqttService;
 
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class MainActivity extends AppCompatActivity {
     private SetpointDatabaseService setpointDatabaseService = new SetpointDatabaseService();
     private TemperatureDatabaseService temperatureDatabaseService = new TemperatureDatabaseService();
@@ -37,5 +43,45 @@ public class MainActivity extends AppCompatActivity {
 
         mqttService = MqttService.getInstance(this);
         mStatusUpdateAt = findViewById(R.id.tv_status_update_at);
+        mTempText = findViewById(R.id.temperature);
+
+        final Gson gson = new Gson();
+
+        mqttService.addListener(new IMqttMessageListener() {
+            @Override
+            public void messageArrived(String topic, MqttMessage message) {
+                final SystemStatus systemStatus = gson.fromJson(new String(message.getPayload()), SystemStatus.class);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Time: " + systemStatus.formattedUpdatedAt());
+                        mTempText.setText(systemStatus.getTemperatureRounded() + "\u2103");
+                        mStatusUpdateAt.setText("Updated at:" + systemStatus.formattedUpdatedAt());
+                    }
+                });
+            }
+        });
+
+        ValueLineChart mCubicValueLineChart = findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
     }
 }
